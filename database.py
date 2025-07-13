cea6fe29d0d29aafd3295e38ff5f0be8d020ee57
Cyprian Fusi <44713120+CyprianFusi@users.noreply.github.com>
2025-07-13 21:01:28 +0100

    edit schemas.py

--- database.py
+++ database.py
@@ -1,22 +1,52 @@
+# import os
+# from sqlalchemy import create_engine
+# from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.orm import sessionmaker
+# from dotenv import load_dotenv
+
+# load_dotenv()
+
+# #SQLALCHEMY_DATABASE_URL = f"postgresql://{os.environ['DATABASE_USER']}:@{os.environ['DATABASE_HOST']}/{os.environ['DATABASE_NAME']}"
+
+# user = os.environ['DATABASE_USER']
+# password = os.environ['DATABASE_PASSWORD']
+# host = os.environ['DATABASE_HOST']
+# port = os.environ['DATABASE_PORT']
+# db_name = os.environ['DATABASE_NAME']
+
+# SQLALCHEMY_DATABASE_URL = f"postgresql://{user}:{password}@{host}:{port}/{db_name}"
+
+# engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Base = declarative_base()
+
+#
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
 
-load_dotenv()
+# For development - use SQLite
+if os.environ.get('ENVIRONMENT') == 'development':
+    SQLALCHEMY_DATABASE_URL = "sqlite:///./todo.db"
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL,
+        connect_args={"check_same_thread": False}
+    )
+else:
+    # For production - use PostgreSQL
+    user = os.environ.get('DATABASE_USER')
+    password = os.environ.get('DATABASE_PASSWORD')
+    host = os.environ.get('DATABASE_HOST')
+    port = os.environ.get('DATABASE_PORT', '5432')
+    database = os.environ.get('DATABASE_NAME')
+    
+    if not all([user, password, host, database]):
+        raise ValueError("Database environment variables are not set")
+    
+    SQLALCHEMY_DATABASE_URL = f"postgresql://{user}:{password}@{host}:{port}/{database}"
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
-#SQLALCHEMY_DATABASE_URL = f"postgresql://{os.environ['DATABASE_USER']}:@{os.environ['DATABASE_HOST']}/{os.environ['DATABASE_NAME']}"
-
-user = os.environ['DATABASE_USER']
-password = os.environ['DATABASE_PASSWORD']
-host = os.environ['DATABASE_HOST']
-port = os.environ['DATABASE_PORT']
-db_name = os.environ['DATABASE_NAME']
-
-SQLALCHEMY_DATABASE_URL = f"postgresql://{user}:{password}@{host}:{port}/{db_name}"
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
